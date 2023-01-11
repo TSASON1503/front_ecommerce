af9fa59905518c0ffee8e6e9244cb6898a811975
Tsason <97116490+TSASON1503@users.noreply.github.com>
2023-01-11 23:23:20 +0200

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -14,7 +14,7 @@ import {
 /* FOR ADDING PRODUCTS TO CART */
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   // FETCHING PRODUCT DATA
-  const { data } = await axios.get(`/api/products/${id}`);
+  const { data } = await axios.get(`https://back-com.onrender.com/api/products/${id}`);
 
   dispatch({
     type: CART_ADD_ITEM,
