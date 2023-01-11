882d895e42ea2939c5f36d96b6f753992beb71de
Sasson <tomer1.sasson@intel.com>
2023-01-11 23:14:53 +0200

    first commit

---
+++ src/components/Product.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+/* REACT-BOOTSTRAP */
+import { Card } from "react-bootstrap";
+
+/* REACT ROUTER */
+import { Link } from "react-router-dom";
+
+/* COMPONENTS */
+import Rating from "./Rating";
+
+function Product({ product }) {
+  return (
+    <Card 
+    className="my-3  rounded" 
+    style={{
+       height: "100%",
+       border: '3px solid rgba(201, 76, 76, 0.3)',
+       }}>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img
+          src={product.image}
+          style={{ height: "200px", objectFit: "cover" }}
+        />
+      </Link>
+
+      <Card.Body
+        className="p-3"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="div">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+            color={"#f8e825"}
+          />
+        </Card.Text>
+        <div className="mintPrice" 
+        style={{
+          position: 'static',
+          border: '3px solid #73AD21',
+          textAlign:'center'
+          }}>
+          <Card.Text as="h3">${product.price}</Card.Text>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
---
+++ src/products.js
@@ -0,0 +1,95 @@
+const products = [
+  {
+    _id: "1",
+    name: "LUGIA",
+    image: "/images/POKÉMON 12 INCH LUGIA PLUSH.jpg",
+    description:
+      "This Pokémon Lugia plush toy is just right for Pokémon fans. This 12 inch Lugia toy is authentically detailed, with soft and cuddly fabrics. The Pokémon Lugia plush toy is perfect for cosy bedrooms or taking on the go. Kids can make up their own Pokémon stories with this cuddly Lugia, imagining Pokémon adventures and acting out their favourite Pokémon battles. About Lugia Lugia is a Psychic Flying Type Pokémon. Lugia does not evolve. Lugia's wings pack devastating power—a light fluttering of its wings can blow apart regular houses. As a result, this Pokémon chooses to live out of sight deep under the sea.",
+    brand: "Pokémon",
+    category: "Toys",
+    price: 19.99,
+    countInStock: 7,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iPhone 11 Pro 256GB Memory",
+    image: "/images/phone.jpg",
+    description:
+      "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
+    brand: "Apple",
+    category: "Electronics",
+    price: 599.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "Cannon EOS 80D DSLR Camera",
+    image: "/images/camera.jpg",
+    description:
+      "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
+    brand: "Cannon",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Sony Playstation 4 Pro White Version",
+    image: "/images/playstation.jpg",
+    description:
+      "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
+    brand: "Sony",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: "5",
+    name: "Logitech G-Series Gaming Mouse",
+    image: "/images/mouse.jpg",
+    description:
+      "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
+    brand: "Logitech",
+    category: "Electronics",
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "6",
+    name: "Amazon Echo Dot 3rd Generation",
+    image: "/images/alexa.jpg",
+    description:
+      "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
+    brand: "Amazon",
+    category: "Electronics",
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: "7",
+    name: "PLAYSTATION 4 500GB - FAIR CONDITION",
+    image: "/images/371803_ps4_a.webp",
+    description:
+      "PlayStation 4 will allow you to experience dynamic and connected gaming, fuelled by powerful graphics and speed that is woven into a social and customisable landscape that expands the enjoyment with a second screen.",
+    brand: "Sony",
+    category: "Electronics",
+    price: 159.99,
+    countInStock: 15,
+    rating: 4,
+    numReviews: 12,
+  },
+];
+
+export default products;
